372aeaf4edfdf5a7b34103f6222ec89203375166
Matthis Perrin <matthis.perrin@gmail.com>
2024-02-23 09:12:19 +0100

    Fix deploy script

--- src/project/scripts/deploy.js
+++ src/project/scripts/deploy.js
@@ -48,9 +48,11 @@ async function run() {
 
   // Deploy each lambda
   const projects = getProjects();
-  const lambdaProjects = projects.map(p => p.lambdaName).filter(Boolean);
+  const lambdaProjects = projects
+    .map(p => p.lambdaName ?? (p.appName ? `${p.appName}_backend` : undefined))
+    .filter(Boolean);
   for (const lambdaName of lambdaProjects) {
-    const lambdaUrl = outputs[`${lambdaName}_function_url`] ?? '';
+    const lambdaUrl = outputs[`${lambdaName}_url`] ?? '';
     console.log(`Deploying lambda ${lambdaName}`, lambdaUrl);
     const tmp = tmpdir();
     const zipPath = join(tmp, randomUUID()) + '.zip';
@@ -67,7 +69,9 @@ async function run() {
   }
 
   // Deploy each website
-  const websiteProjects = projects.map(p => p.websiteName).filter(Boolean);
+  const websiteProjects = projects
+    .map(p => p.websiteName ?? (p.appName ? `${p.appName}_frontend` : undefined))
+    .filter(Boolean);
   for (const websiteName of websiteProjects) {
     const websiteUrl = outputs[`${websiteName}_cloudfront_domain_name`];
     console.log(`Deploying website ${websiteName}`, websiteUrl);
