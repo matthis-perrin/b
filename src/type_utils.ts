b02f202b9bef5d529340a1633895dd21623ab721
Matthis Perrin <matthis.perrin@gmail.com>
2024-06-20 12:06:02 +0200

    Fix asMap + Add triggerDownload function

--- src/type_utils.ts
+++ src/type_utils.ts
@@ -43,7 +43,7 @@ export function asMap(
   value: unknown,
   defaultValue?: Record<string, unknown>
 ): Record<string, unknown> | undefined {
-  return typeof value === 'object' && value !== null
+  return typeof value === 'object' && value !== null && !Array.isArray(value)
     ? (value as Record<string, unknown>)
     : defaultValue;
 }
